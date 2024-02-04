a1c7166be0d92656092a5bc90c7e9b940b817756
renataalexandria <xnightsky2008@gmail.com>
2024-02-04 23:03:03 +0700

    fix login

--- login.js
+++ login.js
@@ -4,6 +4,7 @@ import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10
 
 const loginForm = document.getElementById('loginForm');
 
+
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
@@ -12,24 +13,45 @@ loginForm.addEventListener('submit', (e) => {
 
   const auth = getAuth(app);
 
+  // Sign in with email and password
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
       console.log('Login successful for user:', user.email);
-
-      // Simpan informasi pengguna ke dalam Realtime Database
-      const db = getDatabase(app);
-      const userRef = ref(db, 'users/' + user.uid);
-      set(userRef, {
-        email: user.email,
-        // Tambahkan data lain yang ingin disimpan, misalnya nama pengguna
-      });
-
       alert('Login successful!');
-      window.location.href = 'app/index.php';
+      window.location.href = 'app/index.php';  
     })
     .catch((error) => {
       console.error('Login error:', error.message);
       alert('Login failed. ' + error.message);
     });
 });
+// loginForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   const email = loginForm.email.value;
+//   const password = loginForm.password.value;
+
+//   const auth = getAuth(app);
+
+//   signInWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+//       const user = userCredential.user;
+//       console.log('Login successful for user:', user.email);
+
+//       // Simpan informasi pengguna ke dalam Realtime Database
+//       const db = getDatabase(app);
+//       const userRef = ref(db, 'users/' + user.uid);
+//       set(userRef, {
+//         email: user.email,
+//         // Tambahkan data lain yang ingin disimpan, misalnya nama pengguna
+//       });
+
+//       alert('Login successful!');
+//       window.location.href = 'app/index.php';
+//     })
+//     .catch((error) => {
+//       console.error('Login error:', error.message);
+//       alert('Login failed. ' + error.message);
+//     });
+// });
